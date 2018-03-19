ca85aa5227fe783d2994c9b8300de707d1977c9f
Flemmli97 <Flemmli97@users.noreply.github.com>
2018-03-19 23:29:59 +0100

    .

--- src/main/java/com/flemmli97/improvedmobs/ImprovedMobs.java
+++ src/main/java/com/flemmli97/improvedmobs/ImprovedMobs.java
@@ -21,7 +21,7 @@ public class ImprovedMobs {
 
     public static final String MODID = "improvedmobs";
     public static final String MODNAME = "Better Mob-AI";
-    public static final String VERSION = "1.2.4[1.10.2]";
+    public static final String VERSION = "1.2.5[1.10.2]";
     public static final Logger logger = LogManager.getLogger(ImprovedMobs.MODID);
         
     @Instance
