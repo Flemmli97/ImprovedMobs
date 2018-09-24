9567f620c3a8558472fd804831bdd4572b10c734
Flemmli97 <Flemmli97@users.noreply.github.com>
2018-09-24 17:59:24 +0200

    fix #7, fix client packet issue

--- src/main/java/com/flemmli97/improvedmobs/ImprovedMobs.java
+++ src/main/java/com/flemmli97/improvedmobs/ImprovedMobs.java
@@ -22,7 +22,7 @@ public class ImprovedMobs {
 
     public static final String MODID = "improvedmobs";
     public static final String MODNAME = "Better Mob-AI";
-    public static final String VERSION = "1.2.3[1.11.2]";
+    public static final String VERSION = "1.2.4[1.11.2]";
     public static final Logger logger = LogManager.getLogger(ImprovedMobs.MODID);
         
     @Instance
