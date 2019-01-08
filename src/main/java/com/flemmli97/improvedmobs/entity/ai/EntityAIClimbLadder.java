7a0f48fe0c77af0dbe91ad7198e298fdcbbd3e28
Flemmli97 <Flemmli97@users.noreply.github.com>
2019-01-08 22:03:38 +0100

    hopefully fixes #11

--- src/main/java/com/flemmli97/improvedmobs/entity/ai/EntityAIClimbLadder.java
+++ src/main/java/com/flemmli97/improvedmobs/entity/ai/EntityAIClimbLadder.java
@@ -8,23 +8,26 @@ import net.minecraft.pathfinding.PathPoint;
 
 public class EntityAIClimbLadder extends EntityAIBase{
 
-	EntityLiving living;	
+	private EntityLiving living;	
 	private Path path;
+	
 	public EntityAIClimbLadder(EntityLiving living)
 	{
 		this.living=living;
 		this.setMutexBits(4);
 	}
+	
 	@Override
 	public boolean shouldExecute() {
 		if(!this.living.getNavigator().noPath())
 		{
 			this.path= this.living.getNavigator().getPath();
-			return this.living.isOnLadder();
+			return this.path!=null && this.living.isOnLadder();
 		}
 		return false;
 	}
 	
+	@Override
 	public void updateTask()
 	{
 		int i = this.path.getCurrentPathIndex();
