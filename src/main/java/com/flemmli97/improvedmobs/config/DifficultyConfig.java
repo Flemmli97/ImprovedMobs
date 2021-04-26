b913b46777ba66fe773b6b728e284ce4238f8182
Flemmli97 <Flemmli97@users.noreply.github.com>
2021-04-26 15:09:15 +0200

    fix #60 guardian boats, add finer difficulty control

---
+++ src/main/java/com/flemmli97/improvedmobs/config/DifficultyConfig.java
@@ -0,0 +1,38 @@
+package com.flemmli97.improvedmobs.config;
+
+import com.flemmli97.tenshilib.api.config.IConfigListValue;
+import com.flemmli97.tenshilib.common.utils.SearchUtils;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+
+public class DifficultyConfig implements IConfigListValue<DifficultyConfig> {
+
+    private static final Pair<Float, Float> defaultVal = Pair.of(0f, 1f);
+    private final List<Pair<Float, Float>> vals = Lists.newArrayList();
+
+    @Override
+    public DifficultyConfig readFromString(List<String> ss) {
+        this.vals.clear();
+        for (String s : ss) {
+            String[] parts = s.split("-");
+            if (parts.length != 2)
+                continue;
+            this.vals.add(Pair.of(Float.parseFloat(parts[0]), Float.parseFloat(parts[1])));
+        }
+        this.vals.sort((o1, o2) -> Float.compare(o1.getLeft(), o2.getLeft()));
+        return this;
+    }
+
+    @Override
+    public List<String> writeToString() {
+        List<String> list = Lists.newArrayList();
+        this.vals.forEach(v -> list.add(v.getLeft() + "-" + v.getRight()));
+        return list;
+    }
+
+    public float get(float difficulty) {
+        return SearchUtils.searchInfFunc(this.vals, v -> Float.compare(v.getLeft(), difficulty), defaultVal).getRight();
+    }
+}
---
+++ src/main/java/com/flemmli97/improvedmobs/mixin/GuardianMixin.java
@@ -0,0 +1,22 @@
+package com.flemmli97.improvedmobs.mixin;
+
+import com.flemmli97.improvedmobs.ImprovedMobs;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.GuardianEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class GuardianMixin {
+
+    @Inject(method = "canBeRiddenInWater", at = @At(value = "HEAD"), cancellable = true)
+    private void ridable(CallbackInfoReturnable<Boolean> info){
+        if((Object)this instanceof GuardianEntity && ((GuardianEntity) (Object) this).getPersistentData().getBoolean(ImprovedMobs.waterRiding)) {
+            info.setReturnValue(true);
+            info.cancel();
+        }
+
+    }
+}
