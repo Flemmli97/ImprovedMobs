dd431622abaecf1fea68623a821dba87710be493
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-01-09 21:28:39 +0100

    fix no breaking mobs not jumping

--- common/src/main/java/io/github/flemmli97/improvedmobs/mixin/pathfinding/PathNavigationMixin.java
+++ common/src/main/java/io/github/flemmli97/improvedmobs/mixin/pathfinding/PathNavigationMixin.java
@@ -1,9 +1,11 @@
 package io.github.flemmli97.improvedmobs.mixin.pathfinding;
 
+import io.github.flemmli97.improvedmobs.mixinhelper.INodeBreakable;
 import io.github.flemmli97.improvedmobs.utils.PathFindingUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.level.pathfinder.NodeEvaluator;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,10 +18,13 @@ public abstract class PathNavigationMixin {
 
     @Shadow
     protected Mob mob;
+    @Shadow
+    protected NodeEvaluator nodeEvaluator;
+
 
     @Inject(method = "getGroundY", at = @At(value = "HEAD"), cancellable = true)
     private void noJumpBreakable(Vec3 pos, CallbackInfoReturnable<Double> info) {
-        if (PathFindingUtils.canBreak(new BlockPos(pos), this.mob)) {
+        if (((INodeBreakable) this.nodeEvaluator).canBreakBlocks() && PathFindingUtils.canBreak(new BlockPos(pos), this.mob)) {
             info.setReturnValue(pos.y - 0.5);
             info.cancel();
         }
