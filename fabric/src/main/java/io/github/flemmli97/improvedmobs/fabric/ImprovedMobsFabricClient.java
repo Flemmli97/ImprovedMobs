80100983c329ec7941766e3fde210ab52a2c0279
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-01-10 14:49:07 +0100

    remove packethandler init from client init

--- fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/ImprovedMobsFabricClient.java
+++ fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/ImprovedMobsFabricClient.java
@@ -3,7 +3,6 @@ package io.github.flemmli97.improvedmobs.fabric;
 import io.github.flemmli97.improvedmobs.client.ClientEvents;
 import io.github.flemmli97.improvedmobs.fabric.config.ConfigLoader;
 import io.github.flemmli97.improvedmobs.fabric.config.ConfigSpecs;
-import io.github.flemmli97.improvedmobs.fabric.network.PacketHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
@@ -11,7 +10,6 @@ public class ImprovedMobsFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> ClientEvents.showDifficulty(matrixStack));
-        PacketHandler.register();
         ConfigSpecs.initClientConfig();
         ConfigLoader.loadClient();
     }
