7911404ff81652bb3e2c9b01b6632f64322ac61f
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-12-24 16:22:35 +0100

    update playerex integration

--- fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/integration/difficulty/PlayerEXDifficulty.java
+++ fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/integration/difficulty/PlayerEXDifficulty.java
@@ -1,6 +1,6 @@
 package io.github.flemmli97.improvedmobs.fabric.integration.difficulty;
 
-import com.github.clevernucleus.playerex.api.ExAPI;
+import com.bibireden.playerex.api.attribute.PlayerEXAttributes;
 import io.github.flemmli97.improvedmobs.api.difficulty.DifficultyGetter;
 import io.github.flemmli97.improvedmobs.config.Config;
 import net.minecraft.server.level.ServerLevel;
@@ -10,7 +10,7 @@ public class PlayerEXDifficulty implements DifficultyGetter {
 
     @Override
     public float getDifficulty(ServerLevel level, Vec3 pos) {
-        return DifficultyGetter.getDifficulty(level, pos, p -> (float) ExAPI.PLAYER_DATA.get(p).get(ExAPI.LEVEL) * Config.CommonConfig.playerEXScale);
+        return DifficultyGetter.getDifficulty(level, pos, p -> (float) (p.getAttributeValue(PlayerEXAttributes.LEVEL) * Config.CommonConfig.playerEXScale));
     }
 
     @Override
