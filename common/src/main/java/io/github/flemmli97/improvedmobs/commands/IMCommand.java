8012241a5538feece75036026fab2a1b114d6cc7
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-05-05 16:21:20 +0200

    add simulate and pause command close #298

--- common/src/main/java/io/github/flemmli97/improvedmobs/commands/IMCommand.java
+++ common/src/main/java/io/github/flemmli97/improvedmobs/commands/IMCommand.java
@@ -3,12 +3,13 @@ package io.github.flemmli97.improvedmobs.commands;
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.flemmli97.improvedmobs.config.Config;
 import io.github.flemmli97.improvedmobs.config.EquipmentList;
 import io.github.flemmli97.improvedmobs.difficulty.DifficultyData;
-import io.github.flemmli97.improvedmobs.difficulty.IPlayerDifficulty;
+import io.github.flemmli97.improvedmobs.difficulty.PlayerDifficulty;
 import io.github.flemmli97.improvedmobs.network.PacketHandler;
 import io.github.flemmli97.improvedmobs.platform.CrossPlatformStuff;
 import net.minecraft.ChatFormatting;
@@ -22,6 +23,7 @@ import net.minecraft.server.level.ServerPlayer;
 
 import java.util.Collection;
 
+// TODO: make command feedback translatable (test translation lib a bit more before)
 public class IMCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -34,6 +36,16 @@ public class IMCommand {
                                 .then(Commands.literal("add").then(Commands.argument("val", FloatArgumentType.floatArg()).executes(IMCommand::addDifficultyPlayer)))))
                         .then(Commands.literal("set").then(Commands.argument("val", FloatArgumentType.floatArg()).executes(IMCommand::setDifficulty)))
                         .then(Commands.literal("add").then(Commands.argument("val", FloatArgumentType.floatArg()).executes(IMCommand::addDifficulty)))
+                        .then(Commands.literal("pause")
+                                .then(Commands.literal("player").then(Commands.argument("players", GameProfileArgument.gameProfile()).executes(src -> IMCommand.pauseDifficulty(src, GameProfileArgument.getGameProfiles(src, "players"), true))))
+                                .executes(src -> IMCommand.pauseDifficulty(src, null, true)))
+                        .then(Commands.literal("unpause")
+                                .then(Commands.literal("player").then(Commands.argument("players", GameProfileArgument.gameProfile()).executes(src -> IMCommand.pauseDifficulty(src, GameProfileArgument.getGameProfiles(src, "players"), false))))
+                                .executes(src -> IMCommand.pauseDifficulty(src, null, false)))
+                        .then(Commands.literal("simulate")
+                                .then(Commands.argument("steps", IntegerArgumentType.integer(1))
+                                        .then(Commands.literal("player").then(Commands.argument("players", GameProfileArgument.gameProfile()).executes(src -> IMCommand.simulateDifficulty(src, GameProfileArgument.getGameProfiles(src, "players"), IntegerArgumentType.getInteger(src, "steps")))))
+                                        .executes(src -> IMCommand.simulateDifficulty(src, null, IntegerArgumentType.getInteger(src, "steps")))))
                 ));
     }
 
@@ -62,7 +74,7 @@ public class IMCommand {
         MinecraftServer server = src.getSource().getServer();
         for (GameProfile prof : profs) {
             ServerPlayer player = server.getPlayerList().getPlayer(prof.getId());
-            IPlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
+            PlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
             data.setDifficultyLevel(FloatArgumentType.getFloat(src, "val"));
             CrossPlatformStuff.INSTANCE.sendClientboundPacket(PacketHandler.createDifficultyPacket(DifficultyData.get(server), player), player);
             src.getSource().sendSuccess(() -> Component.literal("Difficulty for " + prof.getName() + " set to " + data.getDifficultyLevel()).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
@@ -75,7 +87,7 @@ public class IMCommand {
         MinecraftServer server = src.getSource().getServer();
         for (GameProfile prof : profs) {
             ServerPlayer player = server.getPlayerList().getPlayer(prof.getId());
-            IPlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
+            PlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
             data.setDifficultyLevel(data.getDifficultyLevel() + FloatArgumentType.getFloat(src, "val"));
             CrossPlatformStuff.INSTANCE.sendClientboundPacket(PacketHandler.createDifficultyPacket(DifficultyData.get(server), player), player);
             src.getSource().sendSuccess(() -> Component.literal("Difficulty for " + prof.getName() + " set to " + data.getDifficultyLevel()).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
@@ -93,6 +105,52 @@ public class IMCommand {
             diff = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player).getDifficultyLevel();
         }
         src.getSource().sendSuccess(() -> Component.literal("Difficulty: " + diff).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
+        return 1;
+    }
+
+    private static int pauseDifficulty(CommandContext<CommandSourceStack> src, Collection<GameProfile> profs, boolean pause) throws CommandSyntaxException {
+        if (profs != null) {
+            MinecraftServer server = src.getSource().getServer();
+            for (GameProfile prof : profs) {
+                ServerPlayer player = server.getPlayerList().getPlayer(prof.getId());
+                PlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
+                data.setPaused(pause);
+            }
+            src.getSource().sendSuccess(() -> Component.literal("Difficulty " + (pause ? "paused" : "unpaused") + " for given players").setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
+            return profs.size();
+        }
+        DifficultyData data = DifficultyData.get(src.getSource().getServer());
+        data.setPaused(pause);
+        src.getSource().sendSuccess(() -> Component.literal("Difficulty " + (pause ? "paused" : "unpaused")).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
+        return 1;
+    }
+
+    private static int simulateDifficulty(CommandContext<CommandSourceStack> src, Collection<GameProfile> profs, int steps) {
+        if (profs != null) {
+            MinecraftServer server = src.getSource().getServer();
+            for (GameProfile prof : profs) {
+                ServerPlayer player = server.getPlayerList().getPlayer(prof.getId());
+                PlayerDifficulty data = CrossPlatformStuff.INSTANCE.getPlayerDifficultyData(player);
+                int i = steps;
+                while (i > 0) {
+                    float current = data.getDifficultyLevel();
+                    data.setDifficultyLevel(current + Config.CommonConfig.increaseHandler.get(current).getRight().start());
+                    i--;
+                }
+                CrossPlatformStuff.INSTANCE.sendClientboundPacket(PacketHandler.createDifficultyPacket(DifficultyData.get(server), player), player);
+            }
+            src.getSource().sendSuccess(() -> Component.literal(String.format("Simulated %s difficulty steps for given players", steps)).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
+            return profs.size();
+        }
+        DifficultyData data = DifficultyData.get(src.getSource().getServer());
+        int i = steps;
+        float current = data.getDifficulty();
+        while (i > 0) {
+            current += Config.CommonConfig.increaseHandler.get(current).getRight().start();
+            i--;
+        }
+        data.setDifficulty(current, src.getSource().getServer());
+        src.getSource().sendSuccess(() -> Component.literal(String.format("Simulated %s difficulty steps globally. Now at %s", steps, data.getDifficulty())).setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)), true);
         return 1;
     }
 }
