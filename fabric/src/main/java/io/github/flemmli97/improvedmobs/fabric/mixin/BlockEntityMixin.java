aa4f6b8beba9f941d8f9abd2a08ceb7dc96348eb
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-05-09 18:23:25 +0200

    fix #315 missing impl oversight

--- fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/BlockEntityMixin.java
+++ fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/BlockEntityMixin.java
@@ -32,4 +32,9 @@ public abstract class BlockEntityMixin implements ContainerOpenAccess {
         }
         this.improvedMobs$OpenedTile.readFromNBT(data);
     }
+
+    @Override
+    public ContainerOpened improvedMobs$getContainerState() {
+        return this.improvedMobs$OpenedTile;
+    }
 }
