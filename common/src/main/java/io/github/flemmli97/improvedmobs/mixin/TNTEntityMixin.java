721efe16e4d5ed123a17c34c0acc886219832fc0
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-05-09 18:22:38 +0200

    fix #314 tnt destruction config not working

--- common/src/main/java/io/github/flemmli97/improvedmobs/mixin/TNTEntityMixin.java
+++ common/src/main/java/io/github/flemmli97/improvedmobs/mixin/TNTEntityMixin.java
@@ -6,10 +6,10 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.PrimedTnt;
-import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,6 +21,9 @@ public abstract class TNTEntityMixin extends Entity implements ITNTThrowable {
         super(type, world);
     }
 
+    @Shadow
+    protected abstract void explode();
+
     @Inject(method = "tick", at = @At(value = "RETURN"), cancellable = true)
     private void modifyExplosion(CallbackInfo info) {
         PrimedTnt tnt = (PrimedTnt) (Object) this;
@@ -28,7 +31,7 @@ public abstract class TNTEntityMixin extends Entity implements ITNTThrowable {
             info.cancel();
             tnt.remove(RemovalReason.KILLED);
             if (!tnt.level.isClientSide)
-                tnt.level.explode(tnt, tnt.getX(), tnt.getY(0.0625D), tnt.getZ(), 4.0F, Explosion.BlockInteraction.BREAK);
+                this.explode();
         }
     }
 
